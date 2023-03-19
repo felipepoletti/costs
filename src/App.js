5101d630db934c3ec781d91c45d0794925052f71
Felipe Poletti <felipepolettig@gmail.com>
2023-03-19 12:59:51 -0300

    Creating and styling navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+
 import Container from './components/layout/Container';
+import NavBar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/newProject">New Project</Link>
-      </div>
+      <NavBar />
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />}  />
+          <Route path="/projects" element={<Projects />}  />
           <Route path="/company" element={<Company />}  />
           <Route path="/contact" element={<Contact />}  />
           <Route path="/newProject" element={<NewProject />}  />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
