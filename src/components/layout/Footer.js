d068e9ee50ea11c65e691450936f6f3f7f88a613
Felipe Poletti <felipepolettig@gmail.com>
2023-03-19 13:44:15 -0300

    Creating and styling footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,25 @@
+import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa';
+
+import styles from './Footer.module.css';
+
 function Footer() {
   return (
-    <p>Footer</p>
+    <footer className={styles.footer}>
+        <ul className={styles.social_list}>
+          <li>
+            <FaFacebook />
+          </li>
+          <li>
+            <FaLinkedin />
+          </li>
+          <li>
+            <FaInstagram />
+          </li>
+        </ul>
+        <p className={styles.copy_right}>
+          <span>Costs</span> &copy; 2023
+        </p>
+    </footer>
   );
 }
 
