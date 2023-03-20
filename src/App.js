2bcfc32c77e93f304b2d39ccb553e52c517e950c
Felipe Poletti <felipepolettig@gmail.com>
2023-03-20 13:18:01 -0300

    Creating home page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
 import NavBar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import Projects from './components/pages/Projects';
 
 function App() {
   return (
