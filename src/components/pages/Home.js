8a8ca2f7988886a3d39f3a266d2e38d3761b8a3a
Felipe Poletti <felipepolettig@gmail.com>
2023-03-20 13:25:40 -0300

    Creating layout link button for all project

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import styles from './Home.module.css';
 import savings from '../../img/savings.svg';
+import LinkButton from '../layout/LinkButton';
 
 function Home() {
   return (
     <section className={styles.home_container}>
       <h1>Welcome to <span>Costs</span></h1>
       <p>Start managing your projects now</p>
-      <a href='/'>Create a project</a>
+      <LinkButton to="/newproject" text="Create a project"/>
       <img src={savings} alt='Costs'/>
     </section>
   );
