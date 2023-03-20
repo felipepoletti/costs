f07e117f95dc541c519ce6c56566ff216c12ec45
Felipe Poletti <felipepolettig@gmail.com>
2023-03-20 13:18:57 -0300

    Translate texts to english

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,9 +4,9 @@ import savings from '../../img/savings.svg';
 function Home() {
   return (
     <section className={styles.home_container}>
-      <h1>Bem-vindo ao <span>Costs</span></h1>
-      <p>Comece a gerenciar os seus projetos agora mesmo.</p>
-      <a href='/'>Criar projeto</a>
+      <h1>Welcome to <span>Costs</span></h1>
+      <p>Start managing your projects now</p>
+      <a href='/'>Create a project</a>
       <img src={savings} alt='Costs'/>
     </section>
   );
