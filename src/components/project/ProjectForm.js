20d647515bdc203d1608422897f247326a53b359
Felipe Poletti <felipepolettig@gmail.com>
2023-03-21 18:53:20 -0300

    Create input component for form

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,6 +1,8 @@
+import styles from './ProjectForm.module.css';
+
 function ProjectForm() {
   return(
-    <form>
+    <form className={styles.form}>
       <div>
         <input type="text" placeholder="Insert the project name" />
       </div>
