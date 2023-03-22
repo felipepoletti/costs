12b695d07694206f73e7aae2caf7784e462ebeed
Felipe Poletti <felipepolettig@gmail.com>
2023-03-22 20:32:57 -0300

    Using API data on frontend

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
@@ -5,6 +6,25 @@ import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css';
 
 function ProjectForm({ btnText }) {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data)
+    })
+    .catch((err) => console.log(err));
+    }, 
+    []
+  );
+
   return(
     <form className={styles.form}>
       <Input 
@@ -19,7 +39,11 @@ function ProjectForm({ btnText }) {
         name="budget"
         placeholder="Insert the project price"
       />
-      <Select name="category_id" text="Select category"/>
+      <Select 
+        name="category_id"
+        text="Select category"
+        options={categories}
+      />
       <SubmitButton text={btnText}/>
     </form>
   );
