7285763d6ed5bbae14bcc6d0d87be8a0d5cc0b09
Felipe Poletti <felipepolettig@gmail.com>
2023-05-12 15:33:00 -0300

    create remove method

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -38,6 +38,21 @@ function Projects() {
     }, 300)
   }, []);
 
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((resp) => resp.json())
+    .then(data => {
+      setProjects(projects.filter((project) => project.id !== id));
+      // message
+    })
+    .catch((err) => console.log(err))
+  }
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
@@ -54,6 +69,7 @@ function Projects() {
               budget={project.budget}
               category={project.category.name}
               key={project.id}
+              handleRemove={removeProject}
             />
           ))}
           {!removeLoading && <Loading />}
