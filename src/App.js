08ebe7d9016f65415b6ac45952a598f142925ac1
Felipe Poletti <felipepolettig@gmail.com>
2023-05-12 16:41:16 -0300

    Starting edition route and page

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
+
 
 import Container from './components/layout/Container';
 import NavBar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-
 function App() {
   return (
     <Router>
       <NavBar />
       <Container customClass="min-height">
         <Routes>
-          <Route path="/" element={<Home />}  />
+          <Route exact path="/" element={<Home />}  />
           <Route path="/projects" element={<Projects />}  />
           <Route path="/company" element={<Company />}  />
           <Route path="/contact" element={<Contact />}  />
           <Route path="/newProject" element={<NewProject />}  />
+          <Route path="/project/:id" element={<Project />}  />
         </Routes>
       </Container>
       <Footer />
